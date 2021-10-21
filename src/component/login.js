b4f9ee1c0270a9cbd766734b8dc36cd4039d9e5e
Chaithra <chaitrafabbigeri9@gmail.com>
2021-10-21 12:43:53 +0530

    Initial commit

---
+++ src/component/login.js
@@ -0,0 +1,22 @@
+import React from'react';
+function Login(){
+    return(
+<div class='col'>
+    
+    <div className="border responsive">
+        <div className="Login">
+            <h2 className="header">SIGN UP TODAY</h2>
+            <input className="username" type="text" placeholder="username"></input>
+            <input className="email" type="text" placeholder="email"></input>
+            <input className="password" type="test" placeholder="password"></input>
+            <button className="button">SUBMIT</button>
+            <p className="paragraph"><a href=""> PRIVACY POLICY|TERMS AND CONDITIONS</a></p>
+            </div>
+
+     </div>
+    </div>
+
+
+    )
+}
+export default Login;
